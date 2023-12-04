f458b64262bcdbe65e1e7fd59b02cbd615bb03a6
shenny85 <shenderson144@ivytech.edu>
2023-12-04 10:31:46 -0500

    started backend and frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import NavBAr from './NavBar';
+import HomePage from './pages/Homepage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesListPage from './pages/ArticlesListPage';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBAr/>
+      <div id="page-body"> 
+      <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/about" element={<AboutPage/>}/>
+      <Route path="/articles" element={<ArticlesListPage/>}/>
+      <Route path="/articles/:articleId" element={<ArticlePage/>}/>
+      </Routes>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,7 @@
+const ArticlesListPage = () => {
+    return ( 
+        <h1>This is the ArticlesList page!</h1>
+     );
+}
+ 
+export default ArticlesListPage;
